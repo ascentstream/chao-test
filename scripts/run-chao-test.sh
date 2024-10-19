8c9dc8158d060a193f5f0af06dce7862d03e670e
liudezhi <liudezhi@ascentstream.com>
2024-10-19 13:53:38 +0800

    Optimize startup scripts

--- scripts/run-chao-test.sh
+++ scripts/run-chao-test.sh
@@ -2,8 +2,13 @@
 BASE_DIR=$(cd $(dirname $0); pwd)
 echo  $BASE_DIR
 export ASP_NAMESPACE=pulsar-cluster
-
-kubectl -n ${ASP_NAMESPACE} run chao-test --image=chao-test:latest --restart=Never --overrides='
+app_root_path="/app/chao-test-1.0-SNAPSHOT"
+kafka_bootstrap_servers="pulsar-asp-broker-headless:9092"
+send_msg_count="1000"
+max_waiting_time="300"
+topic_partition="10"
+main_class="com.ascentsream.tests.kop.AtLeastOnceMessaging"
+spec='
 {
   "spec": {
     "containers": [
@@ -11,7 +16,7 @@ kubectl -n ${ASP_NAMESPACE} run chao-test --image=chao-test:latest --restart=Nev
         "name": "chao-test",
         "image": "chao-test:latest",
         "imagePullPolicy": "Never",
-        "command": ["/bin/sh", "-c", "cd /app/chao-test-1.0-SNAPSHOT/bin && exec sh runserver.sh -Dkafka.bootstrap.servers=pulsar-asp-broker-headless:9092 -Dsend.msg.count=50000 -Dtopic=at-least-once -Dkafka.group.id=group1 com.ascentsream.tests.kop.AtLeastOnceMessaging"],
+        "command": ["/bin/sh", "-c", "cd {{app_root_path}}/bin && exec sh runserver.sh -Dkafka.bootstrap.servers={{kafka_bootstrap_servers}} -Dsend.msg.count={{send_msg_count}} -Dtopic=at-least-once -Dkafka.group.id=group-1 -Dmax.waiting.time={{max_waiting_time}} -Dtopic.partition={{topic_partition}} {{main_class}}"],
        "volumeMounts": [
           {
             "name": "host-path",
@@ -30,3 +35,22 @@ kubectl -n ${ASP_NAMESPACE} run chao-test --image=chao-test:latest --restart=Nev
          ]
        }
   }'
+
+replace_string() {
+    original_string="$1"
+    to_replace="$2"
+    replace_with="$3"
+    updated_string=$(echo "$original_string" | sed "s|${to_replace}|${replace_with}|g")
+    echo "$updated_string"
+}
+
+
+spec=$(replace_string "$spec" "{{app_root_path}}" "$app_root_path")
+spec=$(replace_string "$spec" "{{kafka_bootstrap_servers}}" "$kafka_bootstrap_servers")
+spec=$(replace_string "$spec" "{{send_msg_count}}" "$send_msg_count")
+spec=$(replace_string "$spec" "{{max_waiting_time}}" "$max_waiting_time")
+spec=$(replace_string "$spec" "{{topic_partition}}" "$topic_partition")
+spec=$(replace_string "$spec" "{{main_class}}" "$main_class")
+
+echo "$spec"
+kubectl -n ${ASP_NAMESPACE} run chao-test --image=chao-test:latest --restart=Never --overrides="$spec"
