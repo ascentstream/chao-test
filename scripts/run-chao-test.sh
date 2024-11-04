b77a5b3ae46e721b90ce3e31d58abc9a69924205
liudezhi <liudezhi@ascentstream.com>
2024-11-04 22:27:52 +0800

    fix run-chao-test.sh topic config

--- scripts/run-chao-test.sh
+++ scripts/run-chao-test.sh
@@ -6,7 +6,7 @@ app_root_path=${APP_ROOT_PATH:-"/app/chao-test-1.0-SNAPSHOT"}
 kafka_bootstrap_servers=${KAFKA_BOOTSTRAP_SERVERS:-"pulsar-asp-broker-headless:9092"}
 send_msg_count=${SEND_MSG_COUNT:-"50000"}
 max_waiting_time=${MAX_WAITING_TIME:-"600"}
-topic=${TOPIC:-"at-least-once"}
+topic_name=${TOPIC:-"at-least-once"}
 topic_partition=${TOPIC_PARTITION:-"10"}
 main_class=${MAIN_CLASS:-"com.ascentsream.tests.kop.AtLeastOnceMessaging"}
 
@@ -18,7 +18,7 @@ spec='
         "name": "chao-test",
         "image": "chao-test:latest",
         "imagePullPolicy": "Never",
-        "command": ["/bin/sh", "-c", "cd {{app_root_path}}/bin && exec sh runserver.sh -Dkafka.bootstrap.servers={{kafka_bootstrap_servers}} -Dsend.msg.count={{send_msg_count}} -Dtopic={{topic}} -Dkafka.group.id=group-1 -Dmax.waiting.time={{max_waiting_time}} -Dtopic.partition={{topic_partition}} {{main_class}}"],
+        "command": ["/bin/sh", "-c", "cd {{app_root_path}}/bin && exec sh runserver.sh -Dkafka.bootstrap.servers={{kafka_bootstrap_servers}} -Dsend.msg.count={{send_msg_count}} -Dtopic={{topic_name}} -Dkafka.group.id=group-1 -Dmax.waiting.time={{max_waiting_time}} -Dtopic.partition={{topic_partition}} {{main_class}}"],
        "volumeMounts": [
           {
             "name": "host-path",
@@ -53,6 +53,7 @@ spec=$(replace_string "$spec" "{{send_msg_count}}" "$send_msg_count")
 spec=$(replace_string "$spec" "{{max_waiting_time}}" "$max_waiting_time")
 spec=$(replace_string "$spec" "{{topic_partition}}" "$topic_partition")
 spec=$(replace_string "$spec" "{{main_class}}" "$main_class")
+spec=$(replace_string "$spec" "{{topic_name}}" "$topic_name")
 
 echo "$spec"
 kubectl -n ${ASP_NAMESPACE} run chao-test --image=chao-test:latest --restart=Never --overrides="$spec"
